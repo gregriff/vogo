25553b9a211bd0b6fe26b8ac03ea7d29bacdc5b7
Greg Griffin <greg.griffin2@gmail.com>
2025-11-05 21:00:09 -0500

    mono audio working, removed unness buffer structs

--- cli/internal/audio/playback.go
+++ cli/internal/audio/playback.go
@@ -10,32 +10,6 @@ import (
 	"gopkg.in/hraban/opus.v2"
 )
 
-type PlaybackBuffer struct {
-	buf,
-	decodeBuf []int16
-	samplesDecoded int
-}
-
-// WritePCM moves PCM data from pb.decodeBuf into the main pb.buf and keeps track of
-// the number of samples written. It should be called after decoded PCM has been placed into
-// pb.decodeBuf by libopus (this overwrites old data in pb.decodeBuf).
-func (pb *PlaybackBuffer) WritePCM(numSamples int) {
-	pb.buf = append(pb.buf, pb.decodeBuf[:numSamples]...)
-	pb.samplesDecoded += numSamples
-}
-
-// Flush writes all samples buffered in the pb to the pcm buffer and resets the state.
-func (pb *PlaybackBuffer) Flush(pcm *AudioBuffer) (decoded int) {
-	pcm.mu.Lock()
-	decoded = pb.samplesDecoded
-	pcm.data = append(pcm.data, pb.buf[:pb.samplesDecoded]...)
-	pcm.mu.Unlock()
-
-	pb.buf = pb.buf[pb.samplesDecoded:]
-	pb.samplesDecoded = 0
-	return
-}
-
 func SetupPlayback(pc *webrtc.PeerConnection) (*malgo.AllocatedContext, *malgo.Device) {
 	// configure playback device
 	ctx, _ := malgo.InitContext(nil, malgo.ContextConfig{}, nil)
@@ -83,14 +57,10 @@ func SetupPlayback(pc *webrtc.PeerConnection) (*malgo.AllocatedContext, *malgo.D
 		panic(decErr)
 	}
 
-	pb := PlaybackBuffer{
-		buf:       make([]int16, int(frameSize)*3),
-		decodeBuf: make([]int16, frameSize),
-	}
+	pcmBuffer := make([]int16, int(frameSize)*4)
 
-	// this is where the decoder writes pcm, and what we use to write to the playback buffer
+	// this is where the decoder writes pcm from the network
 	pc.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-		// device.Start()
 		for { // Read RTP packets
 			// track.SetReadDeadline(time.Now().Add(1*time.Second))
 			packet, _, readErr := track.ReadRTP()
@@ -103,20 +73,17 @@ func SetupPlayback(pc *webrtc.PeerConnection) (*malgo.AllocatedContext, *malgo.D
 				continue // Temporary error, keep trying
 			}
 
-			numSamples, decodeErr := decoder.Decode(packet.Payload, pb.decodeBuf)
+			samplesDecoded, decodeErr := decoder.Decode(packet.Payload, pcmBuffer)
 			if decodeErr != nil {
 				log.Println("DECODE ERROR: ", decodeErr.Error())
 				continue
 			}
-			pb.WritePCM(numSamples)
 
-			// if pb.samplesDecoded <= 1000 {
-			// continue
-			// }
-			n := pb.Flush(&pcm)
-			log.Print(" r", n)
-
-			// TODO: could use bytes decoded to know how much to read into the playback device
+			// Write decoded PCM to playback buffer, which malgo will pull from for playback
+			pcm.mu.Lock()
+			pcm.data = append(pcm.data, pcmBuffer[:samplesDecoded]...)
+			pcm.mu.Unlock()
+			log.Print(" r", samplesDecoded)
 		}
 	})
 	return ctx, device
